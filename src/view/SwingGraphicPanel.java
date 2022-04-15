b23ffe34837bddb3123e762fb2c71163ad5b1f49
Nocturnal23 <matarazzogennaro1997@gmail.com>
2022-04-15 13:52:49 +0200

    Abbozzo della parte grafica.

---
+++ src/view/SwingGraphicPanel.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SwingGraphicPanel {
+    public SwingGraphicPanel() {
+        JFrame pane = new JFrame();
+        //pane.setUndecorated(true);
+        pane.setVisible(true);
+        pane.setSize(new Dimension(400, 400));
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        pane.setLocation(dimension.width/2-pane.getSize().width/2, dimension.height/2-pane.getSize().height/2);
+        pane.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
