8b7f9c015c2d11d773d7c8b321483905e5489cb3
Nocturnal23 <matarazzogennaro1997@gmail.com>
2022-04-15 13:57:44 +0200

    Main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import view.SwingGraphicPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingGraphicPanel panel = new SwingGraphicPanel();
+    }
+}
