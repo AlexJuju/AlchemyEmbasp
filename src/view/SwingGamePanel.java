e44cca44b9d6e2ed2944e503313e383225b5b4f3
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2022-04-29 20:13:25 +0200

    Testing

--- src/view/SwingGamePanel.java
+++ src/view/SwingGamePanel.java
@@ -1,6 +1,10 @@
 package view;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class SwingGamePanel extends JPanel {
 
@@ -8,6 +12,17 @@ public class SwingGamePanel extends JPanel {
 
     private SwingGamePanel() {
         this.add(new JLabel("Benvenuto al gioco c:"));
+        File file = new File("src"+File.separator+"resources" + File.separator + "LeoRed.png");
+        Image icona = null;
+        try {
+            icona = ImageIO.read(file);
+        } catch (IOException e) {	e.printStackTrace();	}
+        JButton bottone = new JButton("", new ImageIcon(icona));
+        bottone.setBackground(Color.ORANGE);
+        this.setBorder(BorderFactory.createEmptyBorder(5, 15, 5, 15));
+        this.setLayout( new GridLayout(5, 1, 15, 15) );
+        this.add(bottone);
+//        this.setLayout();
     }
 
     public static JPanel getPanel() {
