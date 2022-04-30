26123fbeaafe5d728f22b9a32a4d7b2bfbe5d6ac
Nocturnal23 <matarazzogennaro1997@gmail.com>
2022-04-30 21:33:48 +0200

    Testing.

--- src/view/SwingGamePanel.java
+++ src/view/SwingGamePanel.java
@@ -1,5 +1,9 @@
 package view;
 
+import commons.Enums;
+import core.GameLogic;
+import core.SwingButtonListener;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -9,19 +13,28 @@ import java.io.IOException;
 public class SwingGamePanel extends JPanel {
 
     private static SwingGamePanel panel;
+    private Button bMatrix[][] = new Button[GameLogic.getHeight()][GameLogic.getWidth()];
 
     private SwingGamePanel() {
-        this.add(new JLabel("Benvenuto al gioco c:"));
-        File file = new File("src"+File.separator+"resources" + File.separator + "LeoRed.png");
-        Image icona = null;
-        try {
-            icona = ImageIO.read(file);
-        } catch (IOException e) {	e.printStackTrace();	}
-        JButton bottone = new JButton("", new ImageIcon(icona));
-        bottone.setBackground(Color.ORANGE);
-        this.setBorder(BorderFactory.createEmptyBorder(5, 15, 5, 15));
-        this.setLayout( new GridLayout(5, 1, 15, 15) );
-        this.add(bottone);
+        this.setLayout(new GridLayout(GameLogic.getWidth(), GameLogic.getHeight(), 1, 1));
+
+        for(int i = 0; i < GameLogic.getHeight()-1; i++)
+            for(int j = 0; j < GameLogic.getWidth()-1; j++) {
+                bMatrix[i][j] = new Button(" w " + j + " h " + i, Enums.ButtonAction.TILE, new SwingButtonListener());
+                this.add(bMatrix[i][j]);
+            }
+
+//        this.add(new JLabel("Benvenuto al gioco c:"));
+//        File file = new File("src"+File.separator+"resources" + File.separator + "LeoRed.png");
+//        Image icona = null;
+//        try {
+//            icona = ImageIO.read(file);
+//        } catch (IOException e) {	e.printStackTrace();	}
+//        JButton bottone = new JButton("", new ImageIcon(icona));
+//        bottone.setBackground(Color.ORANGE);
+//        this.setBorder(BorderFactory.createEmptyBorder(5, 15, 5, 15));
+//        this.setLayout( new GridLayout(5, 1, 15, 15) );
+//        this.add(bottone);
 //        this.setLayout();
     }
 
