f7b336c6a35e62fa58c6ba01365b145b5129072e
Nocturnal23 <matarazzogennaro1997@gmail.com>
2022-05-06 11:16:00 +0200

    Added constructor and string of rune type.

--- src/commons/RuneType.java
+++ src/commons/RuneType.java
@@ -1,7 +1,31 @@
 package commons;
 
 public class RuneType {
-    public enum Shape{ARIES, TAURUS, GEMINI, CANCER, LEO, VIRGO, LIBRA, SCORPIO, SAGITTARIUS, CAPRICORN, AQUARIUS, PISCES}
-    public enum Color{RED, BLUE, GREEN, YELLOW, PINK, ORANGE, TEAL, GREY}
-    public enum Type{NORMAL, STONE}
+    public enum Shape{ARIES("Aries"), TAURUS("Taurus"), GEMINI("Gemini"), CANCER("Cancer"), LEO("Leo"), VIRGO("Virgo"), LIBRA("Libra"), SCORPIO("Scorpio"), SAGITTARIUS("Sagittarius"), CAPRICORN("Capricorn"), AQUARIUS("Aquarius"), PISCES("Pisces");
+        private String shape;
+        Shape(String shape) {
+            this.shape = shape;
+        }
+        public String getName() {
+            return shape;
+        }
+    }
+    public enum Color{RED("Red"), BLUE("Blue"), GREEN("Green"), YELLOW("Yellow"), PINK("Pink"), ORANGE("Orange"), TEAL("Teal"), GREY("Grey");
+        private String color;
+        Color(String color) {
+            this.color = color;
+        }
+        public String getName() {
+            return color;
+        }
+    }
+    public enum Type{NORMAL("Normal"), STONE("Stone");
+        private String type;
+        Type(String type) {
+            this.type = type;
+        }
+        public String getName() {
+            return type;
+        }
+    }
 }
