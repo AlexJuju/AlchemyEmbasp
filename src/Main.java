e21518979857ad86396d5cc07c21c9872552db24
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2022-05-21 18:49:18 +0200

    Added embasp external libraries, project setup

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 import core.GraphicManager;
+import it.unical.mat.embasp.platforms.desktop.DesktopService;
+import it.unical.mat.embasp.specializations.dlv2.desktop.DLV2DesktopService;
 
 public class Main {
     public static void main(String[] args) {
+
+        DesktopService service = new DLV2DesktopService("lib/dlv2.exe");
+//        DesktopService service = new DLV2DesktopService("lib/dlv2");
+
         try {
             GraphicManager.newWindow(GraphicManager.PanelType.SWING);
         } catch (Exception e) {
