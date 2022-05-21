73d149b5d02cdf58fc7e1b8c908ddd6cd2cc58b7
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2022-05-21 19:15:04 +0200

    Initializing ServiceManager, added blank program file, delete useless logs

--- src/embasp/ServiceManager.java
+++ src/embasp/ServiceManager.java
@@ -1,5 +1,8 @@
 package embasp;
 
+import it.unical.mat.embasp.base.InputProgram;
+import it.unical.mat.embasp.base.OptionDescriptor;
+import it.unical.mat.embasp.languages.asp.ASPInputProgram;
 import it.unical.mat.embasp.platforms.desktop.DesktopHandler;
 import it.unical.mat.embasp.platforms.desktop.DesktopService;
 import it.unical.mat.embasp.specializations.dlv2.desktop.DLV2DesktopService;
@@ -7,25 +10,29 @@ import it.unical.mat.embasp.specializations.dlv2.desktop.DLV2DesktopService;
 public class ServiceManager {
     private static DesktopService service;
     private static DesktopHandler handler;
+    private static InputProgram program;
 
     private ServiceManager () {}
 
     public static void initialize() {
-        System.out.println("path: " + _getDLVPath());
         service = new DLV2DesktopService( _getDLVPath() );
+        handler = new DesktopHandler(service);
+        program = new ASPInputProgram();
+        program.addFilesPath("encodings/alchemyia");
+        handler.addProgram(program);
+
+//        OptionDescriptor option = new OptionDescriptor("-n 0");
+//        handler.addOption(option);
     }
 
     private static String _getDLVPath() {
         String system = System.getProperty("os.name");
-        System.out.println(system);
         if (system.contains("Windows")) {
             return "lib/dlv2.exe";
         }
-
         if (system.contains("Linux")) {
             return "lib/dlv2";
         }
-
         return "";
     }
 }
