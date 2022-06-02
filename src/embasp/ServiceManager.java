048082734960066a365e9ad778ed6a3da8cd94e1
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2022-06-02 19:27:41 +0200

    Added gamestate in ServiceManager, DLV classes load

--- src/embasp/ServiceManager.java
+++ src/embasp/ServiceManager.java
@@ -1,8 +1,13 @@
 package embasp;
 
+import embasp.entities.DLVPlace;
+import embasp.entities.DLVPlacedRune;
 import it.unical.mat.embasp.base.InputProgram;
 import it.unical.mat.embasp.base.OptionDescriptor;
+import it.unical.mat.embasp.languages.IllegalAnnotationException;
+import it.unical.mat.embasp.languages.ObjectNotValidException;
 import it.unical.mat.embasp.languages.asp.ASPInputProgram;
+import it.unical.mat.embasp.languages.asp.ASPMapper;
 import it.unical.mat.embasp.platforms.desktop.DesktopHandler;
 import it.unical.mat.embasp.platforms.desktop.DesktopService;
 import it.unical.mat.embasp.specializations.dlv2.desktop.DLV2DesktopService;
@@ -10,19 +15,32 @@ import it.unical.mat.embasp.specializations.dlv2.desktop.DLV2DesktopService;
 public class ServiceManager {
     private static DesktopService service;
     private static DesktopHandler handler;
-    private static InputProgram program;
+    private static InputProgram programrules;
+    private static InputProgram gamestate;
 
     private ServiceManager () {}
 
     public static void initialize() {
         service = new DLV2DesktopService( _getDLVPath() );
         handler = new DesktopHandler(service);
-        program = new ASPInputProgram();
-        program.addFilesPath("encodings/alchemyia");
-        handler.addProgram(program);
+        programrules = new ASPInputProgram();
+        programrules.addFilesPath("encodings/alchemyia");
+        handler.addProgram(programrules);
 
 //        OptionDescriptor option = new OptionDescriptor("-n 0");
 //        handler.addOption(option);
+
+        try {
+            ASPMapper.getInstance().registerClass(DLVPlace.class);
+            ASPMapper.getInstance().registerClass(DLVPlacedRune.class);
+        } catch (ObjectNotValidException | IllegalAnnotationException e1) {
+            e1.printStackTrace();
+        }
+        gamestate = new ASPInputProgram();
+    }
+
+    public static void reloadGamestate () {
+        gamestate.clearAll();
     }
 
     private static String _getDLVPath() {
